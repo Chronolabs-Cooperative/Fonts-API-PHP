39d37f41659aad6324482248b49257c96b037432
Simon Antony Roberts <simon@staff.labs.coop>
2016-06-20 15:18:29 +1000

    Fonting Repository API - Version 2.3.2!

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(E_ERROR);
-set_time_limit(1999);
-require_once __DIR__.'/functions.php';
-print_r(getGlyphArrayFromXML(xml2array(file_get_contents(__DIR__."/examples/glyph.glif"))));
-
-?>
